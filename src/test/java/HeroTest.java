ade74166c2f82b839116a860f11dfeb332914f6b
abbiemurugi <abigaelmurugi@gmail.com>
2019-04-18 05:23:53 +0300

    all tests for heroes

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,9 +1,15 @@
 
+import org.junit.After;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class HeroTest {
+
+    @After
+    public void tearDown(){
+        Hero.clear();
+    }
     @Test
     public void Hero_instantiatesCorrectly_true(){
         Hero myHero = new Hero("SpiderMan", 2000, "lazer eyes", "kryptontite");
@@ -37,15 +43,29 @@ public class HeroTest {
     @Test
     public void all_returnsAllInstancesOfHero_true() {
         Hero firstHero = new Hero("SpiderMan", 2000, "lazer eyes", "kryptontite");
-        Hero secondHero = new Hero("SpiderMan", 2000, "lazer eyes", "kryptontite");
+        Hero secondHero = new Hero("Joker", 40, "genius", "Batman");
         assertEquals(true, Hero.all().contains(firstHero));
         assertEquals(true, Hero.all().contains(secondHero));
     }
 
     @Test
+    public void clear_emptiesAllHeroesFromArrayList_0() {
+        Hero myHero = new Hero("SpiderMan", 2000, "lazer eyes", "kryptontite");
+        Hero.clear();
+        assertEquals(Hero.all().size(), 0);
+    }
+
+    @Test
+    public void getId_tasksInstantiateWithAnID_1() {
+        Hero.clear();  // Remember, the test will fail without this line! We need to empty leftover Hero from previous tests!
+        Hero myHero = new Hero("SpiderMan", 2000, "lazer eyes", "kryptontite");
+        assertEquals(1, myHero.getId());
+    }
+
+    @Test
     public void find_returnHeroWithSameId_secondHero(){
         Hero firstHero = new Hero("SpiderMan", 2000, "lazer eyes", "kryptontite");
-        Hero secondHero = new Hero("SpiderMan", 2000, "lazer eyes", "kryptontite");
+        Hero secondHero = new Hero("Joker", 40, "genius", "Batman");
         assertEquals(Hero.find(secondHero.getId()), secondHero);
     }
 }
